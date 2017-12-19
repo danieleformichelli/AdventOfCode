7fe581455b713c6fab6013d0c808bbd73efb37ae
Daniele Formichelli <daniele.formichelli@gmail.com>
2017-12-19 23:21:02 +0100

    Added day12

---
+++ src/day12/Ex2.kt
@@ -0,0 +1,31 @@
+package day12
+
+import utils.getMultiLineInput
+
+/*
+There are more programs than just the ones in the group containing program ID 0. The rest of them have no way of reaching that group, and still might have no way of reaching each other.
+A group is a collection of programs that can all communicate via pipes either directly or indirectly. The programs you identified just a moment ago are all part of the same group. Now, they would like you to determine the total number of groups.
+In the example above, there were 2 groups: one consisting of programs 0,2,3,4,5,6, and the other consisting solely of program 1.
+*/
+fun main(args: Array<String>) {
+    val input = getMultiLineInput()
+
+    val programMap = mutableMapOf<Int,List<Int>>()
+
+    input.forEach {
+        val split = it.split("<->")
+        val from = split[0].trim().toInt()
+        programMap[from] = split[1].split(",").map { it.trim().toInt() }
+    }
+
+    val programList = programMap.keys
+    var groupCount = 0
+    while (!programList.isEmpty()) {
+        val group = mutableSetOf<Int>()
+        addProgramToSet(group, programMap, programList.first())
+        programList.removeAll(group)
+        ++groupCount
+    }
+
+    println(groupCount)
+}
