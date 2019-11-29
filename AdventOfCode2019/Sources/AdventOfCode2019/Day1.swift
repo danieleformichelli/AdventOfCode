8cafb3dc484baffd73a4e2007c101106e5f8f62b
Daniele Formichelli <df@bendingspoons.com>
2019-11-29 22:00:34 +0100

    chore: fix structure

--- AdventOfCode2019/Sources/AdventOfCode2019/Day1.swift
+++ AdventOfCode2019/Sources/AdventOfCode2019/Day1.swift
@@ -1,9 +1,9 @@
 struct Day1: DayBase {
   func part1(input: String) -> String {
-    return "1"
+    return Utils.readLines(from: input)[0]
   }
 
   func part2(input: String) -> String {
-    return "2"
+    return Utils.readLines(from: input)[1]
   }
 }
--- AdventOfCode2019/Sources/AdventOfCode2019/Utils.swift
+++ AdventOfCode2019/Sources/AdventOfCode2019/Utils.swift
@@ -8,15 +8,7 @@
 import Foundation
 
 enum Utils {
-  static func readSingleLine(from filename: String) -> String {
-    return Self.readLines(from: filename)[0]
-  }
-
-  static func readLines(from filename: String) -> [String] {
-    guard let fileContent = try? String(contentsOfFile: filename) else {
-      return []
-    }
-
-    return fileContent.components(separatedBy: "\n")
+  static func readLines(from input: String) -> [String] {
+    return input.components(separatedBy: "\n")
   }
 }
