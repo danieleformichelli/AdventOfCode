add8687257dcad98dc8130f8ccf2cccc955fa2da
Daniele Formichelli <df@bendingspoons.com>
2019-11-30 21:47:19 +0100

    add more utils methods

--- AdventOfCode2019/AdventOfCode2019/Utils.swift
+++ AdventOfCode2019/AdventOfCode2019/Utils.swift
@@ -12,4 +12,16 @@ enum Utils {
   static func readLines(from input: String) -> [String] {
     return input.components(separatedBy: "\n")
   }
+
+  static func readLinesAsInt(from input: String) -> [Int] {
+    return input.components(separatedBy: "\n").compactMap { Int($0) }
+  }
+
+  static func readSpaceSeparatedInput(from input: String) -> [String] {
+    return input.components(separatedBy: " ")
+  }
+
+  static func readSpaceSeparatedMultiLineInput(from input: String) -> [[String]] {
+    return input.components(separatedBy: "\n").map { $0.components(separatedBy: " ") }
+  }
 }
