2aa43665acd5351da9d3fd9a5a5a9db744850df5
Daniele Formichelli <df@bendingspoons.com>
2020-12-25 15:42:12 +0100

    feat: add day 25

---
+++ Swift/Sources/2020/Year2020Day25.swift
@@ -0,0 +1,49 @@
+//
+//  Year2020Day25.swift
+//  AdventOfCode2020
+//
+//  Created by Daniele Formichelli on 25/12/2020.
+//  Copyright © 2020 Daniele Formichelli. All rights reserved.
+//
+
+import Foundation
+import Utils
+
+/// https://adventofcode.com/2020/day/25
+public struct Year2020Day25: DayBase {
+  public func part1(_ input: String) -> CustomDebugStringConvertible {
+    let publicKeys = input.numbers
+    let loopSizes = publicKeys.map { self.findLoopSize(subject: 7, result: $0) }
+    let encryptionKey1 = self.transform(subject: publicKeys[1], loopSize: loopSizes[0])
+    let encryptionKey0 = self.transform(subject: publicKeys[0], loopSize: loopSizes[1])
+    guard encryptionKey0 == encryptionKey1 else {
+      fatalError()
+    }
+    return encryptionKey1
+  }
+
+  public func part2(_ input: String) -> CustomDebugStringConvertible {
+    return input
+  }
+
+  private func transform(subject: Int, loopSize: Int) -> Int {
+    var result = 1
+    (1 ... loopSize).forEach { _ in
+      result = result * subject % 20201227
+    }
+    return result
+  }
+
+  private func findLoopSize(subject: Int, result expectedResult: Int) -> Int {
+    var result = 1
+    var loopSize = 1
+    while true {
+      result = result * subject % 20201227
+      if result == expectedResult {
+        return loopSize
+      }
+      loopSize += 1
+    }
+    return result
+  }
+}
---
+++ Swift/Tests/2020/Year2020Day25Tests.swift
@@ -0,0 +1,27 @@
+//
+//  Year2020Day25Tests.swift
+//  Tests
+//
+//  Created by Daniele Formichelli on 25/12/20.
+//  Copyright © 2020 Daniele Formichelli. All rights reserved.
+//
+
+@testable import AdventOfCode2020
+import XCTest
+
+class Year2020Day25Tests: XCTestCase {
+  func testPart1() {
+    XCTAssertEqual(Year2020Day25().part1(Self.input).debugDescription, "438".debugDescription)
+  }
+
+  func testPart2() {
+    XCTAssertEqual(Year2020Day25().part2(Self.input).debugDescription, "4038")
+  }
+
+  static var input: String {
+    """
+    13135480
+    8821721
+    """
+  }
+}
