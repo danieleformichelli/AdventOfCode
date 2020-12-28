531a40b655a77d3d63fbb00651e1bde105f739be
Daniele Formichelli <df@bendingspoons.com>
2020-12-28 16:43:30 +0100

    feat: add 2015 day 9

---
+++ Swift/Sources/2015/Year2015Day9.swift
@@ -0,0 +1,70 @@
+//
+//  Year2015Day9.swift
+//
+//  Copyright © 2020 Bending Spoons. All rights reserved.
+//
+
+import Parsing
+import Utils
+
+/// https://adventofcode.com/2015/day/9
+struct Year2015Day9: DayBase {
+  func part1(_ input: String) -> CustomDebugStringConvertible {
+    let distances = input.distances
+    return Self.shortestPath(remainingLocations: distances.allLocations, distances: distances, reduce: { $0.min() })
+  }
+
+  func part2(_ input: String) -> CustomDebugStringConvertible {
+    let distances = input.distances
+    return Self.shortestPath(remainingLocations: distances.allLocations, distances: distances, reduce: { $0.max() })
+  }
+
+  private static func shortestPath(
+    currentLocation: String? = nil,
+    remainingLocations: Set<String>,
+    distances: [Route: Int],
+    reduce: ([Int]) -> Int?
+  ) -> Int {
+    let possibleDistances = remainingLocations
+      .compactMap { destination -> Int? in
+        let possibleDistances = distances
+          .compactMap { route, distance -> Int? in
+            guard route.locations.contains(destination) else { return nil }
+            guard currentLocation == nil || route.locations.contains(currentLocation ?? "") else { return nil }
+
+            let remaining = Self.shortestPath(
+              currentLocation: destination,
+              remainingLocations: remainingLocations.symmetricDifference([destination]),
+              distances: distances,
+              reduce: reduce
+            )
+            return currentLocation != nil ? distance + remaining : remaining
+          }
+        return reduce(possibleDistances)
+      }
+    return reduce(possibleDistances) ?? 0
+  }
+}
+
+fileprivate struct Route: Hashable {
+  let locations: Set<String>
+
+  init(_ location: String, _ otherLocation: String) {
+    self.locations = [location, otherLocation]
+  }
+}
+
+extension String {
+  fileprivate var distances: [Route: Int] {
+    let id = Prefix<Substring>(minLength: 1) { $0.isLetter }.map(\.asString)
+    let distance = id.skip(StartsWith(" to ")).take(id).skip(StartsWith(" = ")).take(Int.parser()).map { (Route($0, $1), $2) }
+    let distances = Many(distance, separator: StartsWith("\n")).parse(self)!
+    return Dictionary(uniqueKeysWithValues: distances)
+  }
+}
+
+extension Dictionary where Key == Route {
+  var allLocations: Set<String> {
+    self.keys.flatMap { $0.locations }.asSet
+  }
+}
---
+++ Swift/Tests/2015/Year2015Day9Tests.swift
@@ -0,0 +1,51 @@
+//
+//  Year2015Day9Tests.swift
+//
+//  Copyright © 2020 Bending Spoons. All rights reserved.
+//
+
+@testable import AdventOfCode2015
+import XCTest
+
+class Year2015Day9Tests: XCTestCase {
+  func testPart1() {
+    XCTAssertEqual(Year2015Day9().part1(Self.input).debugDescription, "207")
+  }
+
+  func testPart2() {
+    XCTAssertEqual(Year2015Day9().part2(Self.input).debugDescription, "804")
+  }
+
+  static var input: String {
+    """
+    Faerun to Norrath = 129
+    Faerun to Tristram = 58
+    Faerun to AlphaCentauri = 13
+    Faerun to Arbre = 24
+    Faerun to Snowdin = 60
+    Faerun to Tambi = 71
+    Faerun to Straylight = 67
+    Norrath to Tristram = 142
+    Norrath to AlphaCentauri = 15
+    Norrath to Arbre = 135
+    Norrath to Snowdin = 75
+    Norrath to Tambi = 82
+    Norrath to Straylight = 54
+    Tristram to AlphaCentauri = 118
+    Tristram to Arbre = 122
+    Tristram to Snowdin = 103
+    Tristram to Tambi = 49
+    Tristram to Straylight = 97
+    AlphaCentauri to Arbre = 116
+    AlphaCentauri to Snowdin = 12
+    AlphaCentauri to Tambi = 18
+    AlphaCentauri to Straylight = 91
+    Arbre to Snowdin = 129
+    Arbre to Tambi = 53
+    Arbre to Straylight = 40
+    Snowdin to Tambi = 15
+    Snowdin to Straylight = 99
+    Tambi to Straylight = 70
+    """
+  }
+}
