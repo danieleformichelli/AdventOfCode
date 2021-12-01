d5de6ec236db8c0ec5011730d9360c4c6cd065f0
Daniele Formichelli <df@bendingspoons.com>
2021-12-01 08:19:00 +0100

    feat: year 2021 day 1

---
+++ Swift/Sources/2021/Year2021Day1.swift
@@ -0,0 +1,40 @@
+//
+//  Year2021Day1.swift
+//  AdventOfCode2020
+//
+//  Created by Daniele Formichelli on 01/12/20.
+//  Copyright © 2020 Daniele Formichelli. All rights reserved.
+//
+
+import Utils
+
+/// https://adventofcode.com/2021/day/1
+struct Year2021Day1: DayBase {
+  func part1(_ input: String) -> CustomDebugStringConvertible {
+    let inputNumbers = input.numbers
+    var previous = Int.max
+    return inputNumbers
+      .filter {
+        let increases = $0 > previous
+        previous = $0
+        return increases
+      }
+      .count
+  }
+
+  func part2(_ input: String) -> CustomDebugStringConvertible {
+    let inputNumbers = input.numbers
+    var previous = inputNumbers.prefix(3)
+    return inputNumbers
+      .dropFirst(3)
+      .filter {
+        let previousWindow = previous.sum
+        previous = previous.dropFirst()
+        previous.append($0)
+        let currentWindow = previous.sum
+        let increases = currentWindow > previousWindow
+        return increases
+      }
+      .count
+  }
+}
